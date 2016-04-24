959645c3cf045c89afa743f79f99dbee3ae9e571
huyle333 <huyle333@bu.edu>
2016-04-24 03:22:58 -0400

    [HL] Added preliminary serial bfs.

---
+++ serial/bfs_serial.cpp
@@ -0,0 +1,100 @@
+/*
+ * bfs_serial.cpp
+ * g++ -o bfs_serial bfs_serial.cpp -lrt
+ */
+
+#include <iostream>
+#include <stdio.h>      /* printf, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+#include <vector>
+#include <queue>
+
+#define VERTICES 10000
+#define EDGES 10000
+
+#define GIG 1000000000
+#define CPG 2.60            // Cycles per GHz -- Adjust to your computer
+
+#include "bfs_serial.h"
+
+using namespace std;
+
+int main() {
+	int i;
+	int j;
+
+	srand(time(NULL));
+
+	// graph represents the matrix
+	int **graph = new int*[VERTICES];
+	for (i = 0; i < VERTICES; i++) {	
+    	graph[i] = new int[VERTICES]; 
+  	}
+
+	int size[VERTICES] = {};
+	
+	// visited contains visited positions
+	int *visited = new int[VERTICES];
+
+	// Load the graph
+	for (i = 0; i < VERTICES; i++) {
+		size[i] = rand() % EDGES;
+
+		// Load the graph with the vertices
+		for (j = 0; j < size[i]; j++) {
+			int vertex = rand() % VERTICES;
+			graph[i][j] = vertex;
+		}
+	}
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+
+	// Breadth first search
+	for (i = 0; i < VERTICES; i++) {
+		if (!visited[i]) {
+			bfs(graph, size, i, visited);
+		}
+	}
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+
+	elapsedTime = diff(time1, time2);
+
+	cout << "Serial BFS" << endl;
+	printf("CPE: %ld\n", (long int)((double)(CPG) * (double)(GIG * elapsedTime.tv_sec + elapsedTime.tv_nsec)));
+
+	return 0;
+}
+
+void bfs(int** graph, int *size, int vertex, int *visited) {
+	visited[vertex] = 1;
+
+	// double-ended queue
+	deque<int> q;
+	q.push_front(vertex);
+	while (q.size()) {
+		vertex = q.front();
+		q.pop_back();
+		int i;
+
+		for (i = 0; i < size[vertex]; i++) {
+			if (!visited[graph[vertex][i]]) {
+				visited[graph[vertex][i]] = 1;
+				q.push_front(graph[vertex][i]);
+			}
+		}
+	}
+}
+
+struct timespec diff(struct timespec start, struct timespec end) {
+	struct timespec temp;
+	if ((end.tv_nsec-start.tv_nsec) < 0) {
+		temp.tv_sec = end.tv_sec-start.tv_sec - 1;
+		temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+	} else {
+		temp.tv_sec = end.tv_sec - start.tv_sec;
+		temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+	}
+	return temp;
+}
