a61f9b70165619b90dfb94f1a0f45da4c40c39cb
huyle333 <huyle333@bu.edu>
2016-05-05 04:34:12 -0400

    [HL] Testing cuda again.

--- cuda/bfs_cuda.cu
+++ cuda/bfs_cuda.cu
@@ -231,13 +231,14 @@ void populate_random(Node* graph_nodes, int* graph_edge, bool *graph_mask, bool 
 }
 
 void populate_known(Node* graph_nodes, int* graph_edge, bool *graph_mask, bool *updating_graph_mask, bool *graph_visited, bool *h_graph_visited) {
-		int i = 0;
+	int i = 0;
 	int j = 0;
 	int len;
 
 	for (i = 0; i < VERTICES; i++) {
 		// GPU transfer
-		graph_nodes[i].no_of_edges = (rand() % (EDGES)) + 1;
+		graph_nodes[i].no_of_edges = VERTICES;
+		graph_edge[i] = i;
 
 		if (i == 0) {
 			graph_nodes[i].start= i;
